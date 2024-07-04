77815b03aa38bca2e966bf4d9bc8d46363b9675d
lolo <manuelcarrau@yahoo.com>
2024-07-04 17:33:50 +0200

    added more stuff to movie rec project

--- PythonProjects/MediaDB/main.py
+++ PythonProjects/MediaDB/main.py
@@ -2,6 +2,7 @@ import sys
 from PySide6 import QtWidgets
 from widgets import MainWindow
 
+print("Running app")
 app = QtWidgets.QApplication(sys.argv)
 
 window = MainWindow()
--- PythonProjects/MediaDB/widgets.py
+++ PythonProjects/MediaDB/widgets.py
@@ -1,15 +1,17 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget
-from Widgets.UI_MWindow import Ui_MainWindow
+from PySide6.QtWidgets import QWidget, QMainWindow
+from Widgets.UI_MWindow import Ui_MainAppWindow
 from Widgets.UI_VideoQuery import Ui_VideoForm
+from Widgets.UI_AboutWindow import Ui_AboutDialog
 
-class MainWindow(QWidget, Ui_MainWindow): 
+class MainWindow(QMainWindow, Ui_MainAppWindow): # QMainWindow is required instead of QWidget, as setCentralWidget is part of the first one.
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("Media Database")
 
-        self.actionMovie.triggered.connect(self.ShowVideoQuery)  # .connect(self.ShowVideoQuery)
+        self.actionMovie.triggered.connect(self.ShowVideoQuery)
+        self.actionAbout.triggered.connect(self.ShowAboutDialog)
 
     def ShowVideoQuery(self):
         print("showing video query")
@@ -17,8 +19,24 @@ class MainWindow(QWidget, Ui_MainWindow):
         self.video_query_window.show()
         self.video_query_window.raise_()  # Bring the window to the front
 
+    def ShowAboutDialog(self):
+        print("Showing About Dialog")
+        self.Show_About_Dialog = AboutDialog()
+        self.Show_About_Dialog.show()
+        self.Show_About_Dialog.raise_()
+
+
 class VideoQuery(QWidget, Ui_VideoForm):
        def __init__(self):
         super().__init__()
         self.setupUi(self) 
-        self.setWindowTitle("Video Query")
+        #self.setWindowTitle("Video Query"
+        self.CancelButton.clicked.connect(self.close)
+
+
+class AboutDialog(QWidget, Ui_AboutDialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.AboutAcceptButton.clicked.connect(self.close)
+    
