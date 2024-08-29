626cc46fcf319916956dbcee1ac8a6bb64a1c104
lolo <manuelcarrau@yahoo.com>
2024-08-30 01:07:45 +0200

    Added plenty of new methods, improved many more...

--- Databases/SQLite/MODULE_SQLite_functions.py
+++ Databases/SQLite/MODULE_SQLite_functions.py
@@ -6,11 +6,7 @@ sys.path.append(r'C:\Users\Lolo\Desktop\Programming\GITRepo\PythonLearn-Resource
 import MODULE_SQLite_functions
 
 TODO:
-    - create "check_db_connection" internal method, where it uses the following code + other error handling techniques...         
-        if self.connector is None or self.cursor is None:
-            print(f"DEBUG: connector or cursor is None, creating connection to db")
-            self.connector = sqlite3.connect(self.db_path)
-            self.cursor = self.connector.cursor()
+    - create "check_db_connection" internal method, wwhere it uses the folloing code + other error handling techniques...         
 '''
 
 class DatabaseHandler:
@@ -35,34 +31,42 @@ class DatabaseHandler:
             self.db_name = self.db_name + ".db"
 
         self.db_path = os.path.join(db_dir, self.db_name)
-
         self.connector = None
         self.cursor = None
         self.database_name = None
         self.main_table_name = None
+        self.db_table_names = []
 
-    def create_db_table(self, tableItems, mainTable_name=None):
+    def create_db_table(self, table_items, table_name=None, verbose=None):
         '''
-        tableItems (dict), key = str, item = SQLdatatype
+        table_items (dict), key = str, item = SQLdatatype
         mainTable_name (str)
 
         '''
-        print(f"Setting database {self.db_name} with direction {self.db_path}")
+        if not self.check_db_existance():
+            print(f"Setting database {self.db_name} with direction {self.db_path}")
+
         self.connector = sqlite3.connect(self.db_path)
-
         self.cursor = self.connector.cursor()
-    
-        if mainTable_name is None:
+
+        if table_name is None and self.main_table_name is None:
             self.main_table_name = 'main_table'
-        else:
-            self.main_table_name = mainTable_name
+            table_name = self.main_table_name
+        elif self.main_table_name is None:
+            self.main_table_name = table_name
+            print(f"Setting the main table name as {table_name}")
 
-        print(f"Creating {self.db_name} database table with name '{self.main_table_name}'...")
+        print(f"Creating {self.db_name} database table with name '{table_name}'...")
 
-        columns = ", ".join([f'"{col}" {dtype}' for col, dtype in tableItems.items()])
+        if verbose:
+            print("Items in table:")
+            for item, types in table_items.items():
+                print(f"\t{item}")
 
+        columns = ", ".join([f'"{col}" {dtype}' for col, dtype in table_items.items()])
+        
         create_table_query = f'''
-        CREATE TABLE IF NOT EXISTS {self.main_table_name} (
+        CREATE TABLE IF NOT EXISTS {table_name} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             {columns}
         )
@@ -107,28 +111,128 @@ class DatabaseHandler:
             bool: True if the database exists, False otherwise.
         """
         if os.path.exists(self.db_path):
-            print(f"db path is: {self.db_path}")
+            #print(f"db path is: {self.db_path}")
             return True 
         else:
+            print(f"Database does not exist.")
             return False
 
-    def insert_data_from_df(self, dataframe):
+    def table_has_items(self, table_name=None):
+        if not table_name:
+            table_name = self.main_table_name
+        
+        self.check_db_connection(connect=True)
+        self.cursor.execute(f"SELECT COUNT(1) FROM {table_name};")
+        return self.cursor.fetchone()[0] > 0
+
+    def insert_data_from_df(self, dataframe,table_name=None):
         import pandas as pd
         if not isinstance(dataframe, pd.DataFrame):
             raise TypeError(f"The variable passed to insert data to database is not dataframe type. It is '{type(dataframe)}'")
+        
+        if not table_name:
+            table_name = self.main_table_name
 
-        dataframe.to_sql(self.main_table_name, self.connector, if_exists='append', index=False)
-        self.connector.commit()
+        try:
+            dataframe.to_sql(table_name, self.connector, if_exists='append', index=False)
+            self.connector.commit()
+            print(f"Data inserted successfully into {table_name}")
+            
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+            self.connector.rollback()
 
     def close_connection(self):
+        print(f"Closing connection to database.")
         if self.cursor:
             self.cursor.close()
             self.cursor = None
         if self.connector:
             self.connector.close()
             self.connector = None
+   
+    def check_db_connection(self, connect=False):
+        if self.connector is None or self.cursor is None:
+            if connect == True:
+                # print(f"DEBUG: Connector or cursor is None, creating connection to db")
+                self.connector = sqlite3.connect(self.db_path)
+                self.cursor = self.connector.cursor()
+
+    def retrieve_rows_as_dicts(self, search_parameters_dict, table_name=None):
+        '''
+            Retrieves data from the specified database table based on the parameters provided 
+                in the search_parameters_dict. Each row of the result is returned as a dictionary 
+                with column names as keys and corresponding row values as values.
             
-    def retrieve_data_as_df(self, tableName=None):
+            Parameters:
+                search_parameters_dict: A dictionary where the key is the column name and 
+                    the value is the value to search for in that column.
+                table_name: (Optional) The name of the table to query. If not provided, 
+                    defaults to the main_table_name.
+
+            Returns: 
+                A list of dictionaries, where each dictionary represents a row from 
+                    the database. Returns None if an error occurs.
+        '''
+
+        if table_name is None:
+            table_name = self.main_table_name
+
+        try:
+            print(f"Retrieving data from {table_name}...")
+            
+            sql_query = f"SELECT * FROM {table_name} WHERE "
+
+            value_list = []
+            n = 0
+            for column, value in search_parameters_dict.items():
+                value_list.append(value)
+                sql_query += str(column) + ' = ?'
+                n += 1
+                if n < len(search_parameters_dict):
+                    sql_query += ' AND '
+            
+            self.cursor.execute(sql_query, value_list)
+            rows = self.cursor.fetchall()
+
+            # Get column names from the cursor description 
+            column_names = [description[0] for description in self.cursor.description]
+
+            # Convert each row to a dictionary
+            result_dicts = [dict(zip(column_names, row)) for row in rows]
+            
+            return result_dicts
+
+        except sqlite3.Error as e:
+            # Print the error message
+            print(f"An error occurred: {e}")
+            return None
+
+    def get_last_table_value_of_columns(self, column_names, columns_to_order, table_name=None):
+        if table_name is None:
+            table_name = self.main_table_name
+        
+        query = f"""
+            SELECT {column_names}
+            FROM {table_name}
+            ORDER BY {columns_to_order} DESC
+            LIMIT 1;
+            """
+        
+        if self.cursor is None:
+            self.connector = sqlite3.connect(self.db_path)
+            self.cursor = self.connector.cursor()
+
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+            
+        # If only one column is selected, return the value directly
+        if len(column_names.split(',')) == 1:
+            return result[0] if result else None
+        else:
+            return result if result else None
+
+    def retrieve_all_data_as_df(self, tableName=None):
         if tableName is None and self.main_table_name is None:
             raise BaseException(f"Database table name and main table name are None.")
         elif tableName is None:
@@ -155,51 +259,85 @@ class DatabaseHandler:
         
         return df
 
-class Unused:
-    def check_db_isupdated(self, newData): #TODO: not implemented nor tested at the moment
-
-        self.connector = sqlite3.connect(self.database_name)
-        self.cursor = self.connector.cursor()
-
-        existing_columns = [description[0] for description in self.cursor.execute("PRAGMA table_info(tasks)").fetchall()]
-        new_columns = list(newData.keys())
+    def check_table_existance(self, table_name, verbose=False):
         
-        return set(existing_columns) == set(new_columns)
+        '''
+        if self.connector == None: 
+            print(f"While checking for connector in table existance, it shows None")
+            
+        '''
+        self.cursor.execute(f"PRAGMA table_info({table_name});")
 
-    def update_db(self, newData): #TODO: not implemented nor tested at the moment
+        # Fetch the results
+        result = self.cursor.fetchall()
 
-        if not self.check_db_isupdated(newData):
-            # Add any new columns
-            for col, dtype in newData.items():
-                self.cursor.execute(f"ALTER TABLE tasks ADD COLUMN {col} {dtype}")
+        # Check if the table exists
+        if result:
+            if verbose: print(f"Table '{table_name}' exists.")
+            return True
+        else:
+            if verbose: print(f"Table '{table_name}' does not exist.")
+            return False
 
-        # To update rows, you'll need to have a method of identifying the rows to update,
-        # such as an ID or another unique identifier. Example given is for adding/updating a row.
-        keys = ", ".join(newData.keys())
-        question_marks = ", ".join(["?" for _ in newData])
-        values = tuple(newData.values())
-        insert_query = f"INSERT INTO tasks ({keys}) VALUES ({question_marks})"
-        self.cursor.execute(insert_query, values)
-        self.connector.commit()
-
-    def insert_data(self, data):
-        """Inserts data into the tasks table."""
-        keys = ", ".join(data.keys())
-        question_marks = ", ".join(["?" for _ in data])
-
-        values = tuple(data.values())
-
-        insert_query = f"INSERT INTO tasks ({keys}) VALUES ({question_marks})"
+    def insert_data_from_dict(self, data_dict, table_name,verbose=False):
         
-        self.cursor.execute(insert_query, values)
-        self.connector.commit()
+        if not table_name:
+            table_name = self.main_table_name
 
-    def update_db(self, update_data, identifier):
-        """Updates a row in the tasks table based on a unique identifier."""
-        set_clause = ", ".join([f"{key} = ?" for key in update_data.keys()])
-        update_values = list(update_data.values())
-        update_values.append(identifier)
-        update_query = f"UPDATE tasks SET {set_clause} WHERE id = ?"
-        self.cursor.execute(update_query, tuple(update_values))
-        self.connector.commit()
+        # Construct the SQL query components
+        keys = ", ".join(data_dict.keys())
+        question_marks = ", ".join(["?" for _ in data_dict])
+        values = tuple(data_dict.values())
+
+        insert_query = f"INSERT INTO {table_name} ({keys}) VALUES ({question_marks})"
+        
+        # Execute the query with error handling
+        try:
+            self.cursor.execute(insert_query, values)
+            self.connector.commit()
+            if verbose: 
+                print(f"Data inserted successfully into {table_name}")
+                print(f"\tinserted: {len(values)} values")
+            
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+            self.connector.rollback()
+
+
+# ==================== NOT USED ====================
+def check_db_isupdated(self, newData): #TODO: not implemented nor tested at the moment
+
+    self.connector = sqlite3.connect(self.database_name)
+    self.cursor = self.connector.cursor()
+
+    existing_columns = [description[0] for description in self.cursor.execute("PRAGMA table_info(tasks)").fetchall()]
+    new_columns = list(newData.keys())
     
+    return set(existing_columns) == set(new_columns)
+
+def update_db(self, newData): #TODO: not implemented nor tested at the moment
+
+    if not self.check_db_isupdated(newData):
+        # Add any new columns
+        for col, dtype in newData.items():
+            self.cursor.execute(f"ALTER TABLE tasks ADD COLUMN {col} {dtype}")
+
+    # To update rows, you'll need to have a method of identifying the rows to update,
+    # such as an ID or another unique identifier. Example given is for adding/updating a row.
+    keys = ", ".join(newData.keys())
+    question_marks = ", ".join(["?" for _ in newData])
+    values = tuple(newData.values())
+    insert_query = f"INSERT INTO tasks ({keys}) VALUES ({question_marks})"
+    self.cursor.execute(insert_query, values)
+    self.connector.commit()
+
+
+
+def update_db(self, update_data, identifier):
+    """Updates a row in the tasks table based on a unique identifier."""
+    set_clause = ", ".join([f"{key} = ?" for key in update_data.keys()])
+    update_values = list(update_data.values())
+    update_values.append(identifier)
+    update_query = f"UPDATE tasks SET {set_clause} WHERE id = ?"
+    self.cursor.execute(update_query, tuple(update_values))
+    self.connector.commit()
