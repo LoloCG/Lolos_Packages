3ccf8c7b3106bc703247af8e0c89808424ed399a
lolo <manuelcarrau@yahoo.com>
2024-09-23 10:58:18 +0200

    updated setup files

--- personalpythonpackage/Databases/__init__.py
+++
@@ -1,1 +0,0 @@
-from .sqlite_utils import DatabaseHandler
--- setup.py
+++ setup.py
@@ -2,21 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="PersonalPythonPackage",
-    version="0.1.0",
+    version="0.2.0",
     author="LoloCG",
     author_email="",
     description="My own collection of reusable functions and classes for several python libraries and other...",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/LoloCG/PersonalPythonPackage",  # Your repository URL
-    packages=find_packages(),  # Automatically finds your submodules
-    install_requires=[],  # Base dependencies (if any): "numpy", "pandas", ...
+    url="https://github.com/LoloCG/PersonalPythonPackage",
+    packages=find_packages(include=['personalpythonpackage', 'personalpythonpackage.*']),
+    install_requires=[],
     extras_require={
-        'sqlite': ['pysqlite3'],  # SQLite-related dependencies
-        'pandas': ['pandas'],  # Pandas-only functionality
-        'plotting': ['numpy', 'pandas', 'matplotlib', 'xlsxwriter'],  # Plotting functionality
-        'pyside': ['pyside6'],  # PySide for UI
-        'curses': ['curses'],  # Curses for terminal UIs
+        'pandas': ['pandas'], 
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -25,3 +21,6 @@ setup(
     ],
     python_requires='>=3.6',
 )
+
+
+
