2a1bb1a377011729a41fbfb2f9283be185747210
lolo <manuelcarrau@yahoo.com>
2024-09-26 00:28:03 +0200

    fixed __init__,py

--- Databases/SQLite_ORM/SQLite_ORM/__init__.py
+++ Databases/SQLite_ORM/SQLite_ORM/__init__.py
@@ -1,2 +1,2 @@
-from .basics import ORMManager, DBConnector, TableManager, CRUDManager, dict_to_sqlType
+from .basics import DBManager, Connector, TableManager, CRUDManager, dict_to_sqlType
 from .pandas_addon import insert_data_from_df
