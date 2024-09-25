415a06f49b898d8b2bbb79cf7316af11e9016507
lolo <manuelcarrau@yahoo.com>
2024-09-25 22:51:48 +0200

    deleted older files

--- Databases/SQLite_ORM/SQLite_ORM/pandas_addon.py
+++ Databases/SQLite_ORM/SQLite_ORM/pandas_addon.py
@@ -1,21 +1,4 @@
-def insert_data_from_df(dataframe, connector_obj, table_name):
-    import pandas as pd
-    import sqlite3
-
-    if not isinstance(dataframe, pd.DataFrame):
-        raise TypeError(f"The variable passed to insert data to database is not dataframe type. It is '{type(dataframe)}'")
-
-    connection = connector.conn
-
-    try:
-        dataframe.to_sql(table_name, connection, if_exists='append', index=False)
-        connection.commit()
-        
-    except sqlite3.Error as e:
-        print(f"An error occurred: {e}")
-        connection.rollback()
-
-def retrieve_all_data_as_df(self, tableName=None): # STILL NOT DONE
+def retrieve_all_data_as_df(self, tableName=None): # Still not tested
     if tableName is None and self.main_table_name is None:
         raise BaseException(f"Database table name and main table name are None.")
     elif tableName is None:
@@ -41,4 +24,20 @@ def retrieve_all_data_as_df(self, tableName=None): # STILL NOT DONE
     df = pd.DataFrame(rows, columns=column_names)
     
     return df
+
+def insert_data_from_df(self, dataframe,table_name=None, verbose=False):
+    import pandas as pd
+    if not isinstance(dataframe, pd.DataFrame):
+        raise TypeError(f"The variable passed to insert data to database is not dataframe type. It is '{type(dataframe)}'")
     
+    if not table_name:
+        table_name = self.main_table_name
+
+    try:
+        dataframe.to_sql(table_name, self.connector, if_exists='append', index=False)
+        self.connector.commit()
+        if verbose: print(f"Data inserted successfully into {table_name}")
+        
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+        self.connector.rollback()
