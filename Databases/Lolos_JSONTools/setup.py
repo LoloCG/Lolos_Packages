39c474383e9d59efe968e4bf87692a6535aaaf4a
lolo <manuelcarrau@yahoo.com>
2024-10-01 15:33:11 +0200

    start of JSON tools

---
+++ Databases/Lolos_JSONTools/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Lolos_JSONTools",
+    version="v1.0",
+    author="LoloCG",
+    author_email="",
+    description="Simple reusable functios for native python JSON module",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/LoloCG/Lolos_Packages",
+    packages=find_packages(),
+    install_requires=[],
+    # extras_require={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
+
