e5233d34760e499469b289cf85ef4d822ae98af7
lolo <manuelcarrau@yahoo.com>
2024-10-03 23:55:51 +0200

    added configuration and other functs

--- Python_Logger/PyLogger/basic_logger.py
+++ Python_Logger/PyLogger/basic_logger.py
@@ -1,33 +1,53 @@
 import logging.config
 
-logger = logging.getLogger(__name__)
+def ez_logger(level='INFO'):
+	logger = setup_logger()
+	set_logger_config(level)
+	set_third_party_loggers_level()
+	return logger
 
-# example logging config
-logging_config = {
-	'version': 1,
-	'disable_existing_loggers': False, # False allows third party logs
-	# 'filters': {},
-	'formatters': {
-		'datetimesecs': {
-            'format': "{asctime} - {levelname}: {message}",
-            'style': "{",
-            'datefmt': "%Y-%m-%d %H:%M:%S",
-		}
-	},
-	'handlers': {
-		'stdout': {
-			'class':'logging.StreamHandler', 
-			'formatter': 'datetimesecs',
-			'stream' : 'ext://sys.stdout', # external ???
-		}
-	},
-	'loggers': {
-		'root': {
-			'level': 'DEBUG',
-			'handlers': ['stdout'],
-		}
-	},
-}
+def setup_logger(logger_name=__name__):
+	logger = logging.getLogger(logger_name)
+	return logger
 
-logging.config.dictConfig(config=logging_config)
-logger.warning("debug mesg")
+def print_active_loggers():
+    for name, logger in logging.root.manager.loggerDict.items():
+        if isinstance(logger, logging.Logger):
+            print(f"Logger: {name}, Level: {logging.getLevelName(logger.level)}")
+
+def set_third_party_loggers_level(level='ERROR', exceptions=['core.logger',__name__], exception_level='DEBUG'):
+    for name, logger in logging.root.manager.loggerDict.items():
+        if isinstance(logger, logging.Logger):
+            if name in exceptions:
+                logging.getLogger(name).setLevel(getattr(logging, exception_level))
+            else:
+                logging.getLogger(name).setLevel(getattr(logging, level))
+
+def set_logger_config(level='DEBUG', third_party=False):
+	logging_config = {
+		'version': 1,
+		'disable_existing_loggers': third_party, # False allows third party logs
+		# 'filters': {},
+		'formatters': {
+			'basic': {
+				'format': "{asctime} - {levelname} - {filename}: {message}",
+				'style': "{",
+				'datefmt': "%H:%M:%S",
+			}
+		},
+		'handlers': {
+			'stdout': {
+				'class':'logging.StreamHandler', 
+				'formatter': 'basic',
+				'stream' : 'ext://sys.stdout', # external ???
+			}
+		},
+		'loggers': {
+			'root': {
+				'level': level,
+				'handlers': ['stdout'],
+			}
+		},
+	}
+
+	logging.config.dictConfig(config=logging_config)
