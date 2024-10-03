abcbfcface7ac74c31b312569689cd17953257d3
lolo <manuelcarrau@yahoo.com>
2024-10-03 19:22:12 +0200

    semi functional very basic logger

--- Python_Logger/PyLogger/basic_logger.py
+++ Python_Logger/PyLogger/basic_logger.py
@@ -1,20 +1,33 @@
-import logging
+import logging.config
 
 logger = logging.getLogger(__name__)
 
-# instantiation
-console_handler = logging.StreamHandler() # sends logs to console
-file_handler = logging.FileHandler( # sends log to the file 
-    "app.log",  # file name
-    mode="a",   # append
-    encoding="utf-8")
-# addition to the logger
+# example logging config
+logging_config = {
+	'version': 1,
+	'disable_existing_loggers': False, # False allows third party logs
+	# 'filters': {},
+	'formatters': {
+		'datetimesecs': {
+            'format': "{asctime} - {levelname}: {message}",
+            'style': "{",
+            'datefmt': "%Y-%m-%d %H:%M:%S",
+		}
+	},
+	'handlers': {
+		'stdout': {
+			'class':'logging.StreamHandler', 
+			'formatter': 'datetimesecs',
+			'stream' : 'ext://sys.stdout', # external ???
+		}
+	},
+	'loggers': {
+		'root': {
+			'level': 'DEBUG',
+			'handlers': ['stdout'],
+		}
+	},
+}
 
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
-
-# handlers can be seen with:
-print(logger.handlers)
-
-# test of logging
-logger.warning("Test1")
+logging.config.dictConfig(config=logging_config)
+logger.warning("debug mesg")
